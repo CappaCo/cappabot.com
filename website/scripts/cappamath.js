aea073f0435386f52478c8fc406aa480fdc97058
Casper Howse <casperhowse@gmail.com>
2025-08-25 11:36:53 +1200

    Make default options work

--- website/scripts/cappamath.js
+++ website/scripts/cappamath.js
@@ -343,6 +343,7 @@ function varToMath(name) {
 }
 
 function compareObjectKeys(obj1, obj2) {
+    if (!obj1 || !obj2) return false;
     const keys1 = Object.keys(obj1);
     const keys2 = Object.keys(obj2);
     
